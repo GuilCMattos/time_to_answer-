f49065cdd9010510f013fcebbad8cd6e63e704b2
Guilherme <mttsguil@gmail.com>
2022-11-23 14:22:57 -0300

    add: working with datas and i18n + route for backoffice/admin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_23_155625) do
+ActiveRecord::Schema.define(version: 2019_05_23_002638) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
